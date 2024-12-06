aedc2c15161a93b990601d4dcf51cd1c6c343640
MuhammadHamzaMoosani <hamzamoosani@hotmail.com>
2024-12-07 00:05:35 +0500

    frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,4 +9,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blogApp';
+  authors: any[] = [];
+  blogs: any[] = [];
+  allBlogs: any[] = [];
+  selectedAuthor: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchAuthors();
+    this.fetchAllBlogs();
+  }
+
+  // Fetch all authors
+  fetchAuthors() {
+    this.http.get<any[]>('http://localhost:400/api/authors').subscribe({
+      next: (data: any[]) => (this.authors = data),
+      error: (err: any) => console.error('Error fetching authors:', err)
+    });
+  }
+
+  // Fetch all blogs
+  fetchAllBlogs() {
+    this.http.get<any[]>('http://localhost:400/api/blogs').subscribe({
+      next: (data: any[]) => (this.allBlogs = data),
+      error: (err: any) => console.error('Error fetching all blogs:', err)
+    });
+  }
+
+  // Fetch blogs by selected author
+  onAuthorSelect(authorForm: any) {
+    if (!this.selectedAuthor) return;
+    this.http.get<any[]>(`http://localhost:400/api/blogs/author/${this.selectedAuthor}`).subscribe({
+      next: (data: any[]) => (this.blogs = data),
+      error: (err: any) => console.error('Error fetching blogs by author:', err)
+    });
+  }
+
+  // Trigger adding a new blog
+  onAddBlog() {
+    console.log('Add blog functionality to be implemented');
+  }
 }
